8e97777dac080fee6f3e3b3cc936a5a982d71b92
Cleaux Nathan <nathan.cleaux@etu.univ-lyon1.fr>
2023-02-28 12:05:05 +0100

    color

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,5 +1,36 @@
 <template>
   <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Expand at lg</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExample05">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown05">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-md-0">
+          <input class="form-control" type="text" placeholder="Search">
+        </form>
+      </div>
+    </nav>
   </header>
   
   <main>
@@ -16,6 +47,16 @@
 
 
 <style>
+:root {
+    --primary : #e75112;
+    --secondary: #FDAF17;    
+    --third: #9B9B9B;
+    --bg-primary: #FFFFFF;
+    --bg-secondary: #F7F7F7;
+    --bg-third: #CCCCCC;
+    --bg-navbar: #F1F1F1;
+}
+
 * {
   padding: 0;
   margin: 0;
@@ -57,6 +98,15 @@ footer{
   margin-top: 2%;
   font-family: 'Mukta', sans-serif;   
   bottom: 0;
+}
+
+.navbar {
+    position: relative;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0.5rem 1rem !important;
 }
 
 .link{
