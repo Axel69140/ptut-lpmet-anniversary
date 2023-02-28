249bc61a76293779a19e95b9984e3d0566f1730c
Cleaux Nathan <nathan.cleaux@etu.univ-lyon1.fr>
2023-02-28 09:17:20 +0100

    api usercontroller add

--- back/src/Controller/UserController.php
+++ back/src/Controller/UserController.php
@@ -9,11 +9,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-#[Route('/user')]
 class UserController extends AbstractController
 {
-    #[Route('/', name: 'app_user_index', methods: ['GET'])]
+    /*#[Route('/', name: 'app_user_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
         return $this->render('user/index.html.twig', [
@@ -74,5 +75,88 @@ class UserController extends AbstractController
         }
 
         return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+    }*/
+
+
+    /**
+     * @Route("/api/user", methods={"GET"})
+     */
+    public function index(SerializerInterface $serializer, UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+
+        $json = $serializer->serialize($users, 'json', ['groups' => 'users']);
+
+        return new Response($json, 200, [
+            'Content-Type' => 'application/json'
+        ]);
+    }
+
+    /**
+     * @Route("/api/user", methods={"POST"})
+     */
+    public function create(Request $request, SerializerInterface $serializer, ValidatorInterface $validator): Response
+    {
+        $json = $request->getContent();
+
+        $user = $serializer->deserialize($json, User::class, 'json');
+
+        $errors = $validator->validate($produit);
+
+        if (count($errors) > 0) {
+            return new Response($serializer->serialize($errors, 'json'), 400, [
+                'Content-Type' => 'application/json'
+            ]);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        $json = $serializer->serialize($user, 'json', ['groups' => 'user']);
+
+        return new Response($json, 201, [
+            'Content-Type' => 'application/json'
+        ]);
+    }
+
+    /**
+     * @Route("/api/user/{id}", methods={"PUT"})
+     */
+    public function update(Request $request, SerializerInterface $serializer, ValidatorInterface $validator, User $user): Response
+    {
+        $json = $request->getContent();
+
+        $user = $serializer->deserialize($json, User::class, 'json', ['object_to_populate' => $user]);
+
+        $errors = $validator->validate($user);
+
+        if (count($errors) > 0) {
+            return new Response($serializer->serialize($errors, 'json'), 400, [
+                'Content-Type' => 'application/json'
+            ]);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        $json = $serializer->serialize($user, 'json', ['groups' => 'user']);
+
+        return new Response($json, 200, [
+            'Content-Type' => 'application/json'
+        ]);
+    }
+
+    /**
+     * @Route("/api/user/{id}", methods={"DELETE"})
+     */
+    public function delete(User $user): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($user);
+        $em->flush();
+
+        return new Response(null, 204);
     }
 }
