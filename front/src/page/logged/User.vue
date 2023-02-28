5df4c6a0ccbe5ae4f6a992afb38902376383795f
Rongier Charles <charles.rongier@etu.univ-lyon1.fr>
2023-02-28 17:32:09 +0200

    avancement user

---
+++ front/src/page/logged/User.vue
@@ -0,0 +1,23 @@
+<script>
+    import axios from 'axios';    
+
+    export default {
+        name: 'user',
+        data: () => ({ users: [] }),       
+        computed: {            
+        },
+        methods: {      
+        },
+        mounted() {       
+        }
+    }
+</script>
+
+
+<template>
+    <h1>User</h1>    
+</template>
+
+<style scoped>
+
+</style>
