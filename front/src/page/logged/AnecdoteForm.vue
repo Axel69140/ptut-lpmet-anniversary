f6111c5e00ffb6e1d862e7174fa770bf330d3e46
Rongier Charles <charles.rongier@etu.univ-lyon1.fr>
2023-03-06 17:39:41 +0100

    anecdote start

--- front/src/page/logged/AnecdoteForm.vue
+++ front/src/page/logged/AnecdoteForm.vue
@@ -15,9 +15,44 @@
 
 
 <template>
-    <h1>Proposer une anecdote</h1>    
+    <h1>Proposer une anecdote</h1> 
+    <div class="formulaire">
+        <div class="information">
+            <p class="moreInformation">Les annecdotes sont anonymes</p>
+            <p class="moreInformation">Elles seront projeté lors de l'</p>
+        </div>
+        <div class="divTextZone">
+            <textarea class="textZone" rows="10" cols="100"></textarea>
+        </div>
+        <div class="sendButton">
+            <button class="validButton">Envoyer l'anecdote</button>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+    .formulaire{
+        display: flex;
+        flex-direction: column;
+    }
 
+    .divTextZone{
+        margin: 20px 0;
+    }
+    .textZone{
+        border-radius: 20px;
+        padding: 1%;
+    }
+
+    .validButton{
+        border: 1px solid black;
+        background-color: green;
+        border-radius: 15px;
+        padding: 0.5%;
+        transition: all .2s ease-in-out; 
+    }
+
+    .validButton:hover{
+        transform: scale(1.1);
+    }
 </style>
