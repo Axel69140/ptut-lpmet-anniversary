fec358b615a0a10d36b34da33b8d0c1f68f1ae27
Axel_69140 <>
2023-03-09 12:21:48 +0100

    userController work

--- back/src/Controller/UserController.php
+++ back/src/Controller/UserController.php
@@ -2,55 +2,89 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use App\Service\RequestService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/users')]
 class UserController extends AbstractController
 {
     // API route
     #[Route('/', name: 'app_api_user_get', methods: ['GET'])]
-    public function getUsers(SerializerInterface $serializer, UserRepository $userRepository): Response
+    public function getUsers(UserRepository $userRepository): JsonResponse
     {
         $users = $userRepository->findAll();
 
-        $json = $serializer->serialize($users, 'json');
-
-        return new Response($json, 200, [
-            'Content-Type' => 'application/json'
-        ]);
+        return $this->json([
+            'Content-Type' => $users
+        ], 200);
     }
 
     #[Route('/register', name: 'app_api_user_post', methods: ['POST'])]
-    public function register(Request $request, SerializerInterface $serializer, ValidatorInterface $validator, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, RequestService $requestService): JsonResponse
     {
-        $json = $request->getContent();
+        $content = json_decode($request->getContent(), true);
 
-        $user = $serializer->deserialize($json, User::class, 'json');
-
-        $errors = $validator->validate($user);
-
-        if (count($errors) > 0) {
-            return new Response($serializer->serialize($errors, 'json'), 400, [
-                'Content-Type' => 'application/json'
-            ]);
+        /* Try to access essentials keys */
+        try {
+            $email = $content['email'];
+            $password = $content['password'];
+            $firstName = $content['firstName'];
+            $lastName = $content['lastName'];
+            $isPublicProfil = $content['isPublicProfil'];
+            $activeYears = $content['activeYears'];
+        } catch (\Exception $e) {
+            return $this->json([
+                'error_message' => 'Missing essential key'
+            ], 400);
         }
 
-        $user->setPassword(
-            $userPasswordHasher->hashPassword(
-                $user,
-                json_decode($json)->password
-            )
-        );
-        $entityManager->persist($user);
-        $entityManager->flush();
+        /* Check if essentials keys are not null ("" values) */
+        if (empty($email) || empty($password) || empty($firstName) || empty($lastName) || ($isPublicProfil === null) || empty($activeYears)) {
+            return $this->json([
+                'error_message' => 'A required field is missing'
+            ], 400);
+        } elseif (!is_string($email) || !is_string($password) || !is_string($firstName) || !is_string($lastName) || !is_bool($isPublicProfil) || !is_array($activeYears)) {
+            return $this->json([
+                'error_message' => 'One or more filled-in field(s) has/have a wrong type'
+            ], 400);
+        }
 
-        $json = $serializer->serialize($user, 'json', ['groups' => 'user']);
 
-        return new Response($json, 201, [
-            'Content-Type' => 'application/json'
-        ]);
+
+        return $this->json([
+            'Content-Type' => 'ok'
+        ], 200);
+
+//        $postalCode = $request->query->get('postalCode');
+//
+//        $json = $request->getContent();
+//
+//        $user = $serializer->deserialize($json, User::class, 'json');
+//
+//        $errors = $validator->validate($user);
+//
+//        if (count($errors) > 0) {
+//            return new Response($serializer->serialize($errors, 'json'), 400, [
+//                'Content-Type' => 'application/json'
+//            ]);
+//        }
+//
+//        $user->setPassword(
+//            $userPasswordHasher->hashPassword(
+//                $user,
+//                json_decode($json)->password
+//            )
+//        );
+//        $entityManager->persist($user);
+//        $entityManager->flush();
+//
+//        $json = $serializer->serialize($user, 'json', ['groups' => 'user']);
+//
     }
 
     #[Route('/{id}', name: 'app_api_user_patch', methods: ['PATCH'])]
