5e7d9234902dcc36606c82a0d8ba1a99313649d0
Cleaux Nathan <nathan.cleaux@etu.univ-lyon1.fr>
2023-03-09 15:36:01 +0100

    ok

--- front/src/main.js
+++ front/src/main.js
@@ -3,7 +3,16 @@ import App from './App.vue';
 import { router } from './routes';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import Vue3EasyDataTable from 'vue3-easy-data-table';
+import 'vue3-easy-data-table/dist/style.css';
 import store from './store';
 import jquery from 'jquery'
 
-createApp(App).use(router).use(store).use(jquery).mount('#app');
+//createApp(App).use(router).use(store).use(jquery).mount('#app');
+const app = createApp(App);
+
+app.use(router).use(store).use(jquery);
+
+app.component('EasyDataTable', Vue3EasyDataTable);
+
+app.mount('#app');
