5dda75a3541f7d9490a83272bdf2f61d30597b7d
CLEAUX NATHAN p2000897 <nathan.cleaux@etu.univ-lyon1.fr>
2023-03-18 16:05:44 +0100

    users api work

--- back/src/Controller/UserController.php
+++ back/src/Controller/UserController.php
@@ -10,118 +10,209 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 #[Route('/users')]
 class UserController extends AbstractController
 {
-    // API route
+    // Get users
     #[Route('/', name: 'app_api_user_get', methods: ['GET'])]
     public function getUsers(UserRepository $userRepository): JsonResponse
     {
-        $users = $userRepository->findAll();
-
-        return $this->json([
-            'Content-Type' => $users
-        ], 200);
-    }
-
-    #[Route('/register', name: 'app_api_user_post', methods: ['POST'])]
-    public function register(Request $request, RequestService $requestService): JsonResponse
-    {
-        $content = json_decode($request->getContent(), true);
-
-        /* Try to access essentials keys */
         try {
-            $email = $content['email'];
-            $password = $content['password'];
-            $firstName = $content['firstName'];
-            $lastName = $content['lastName'];
-            $isPublicProfil = $content['isPublicProfil'];
-            $activeYears = $content['activeYears'];
+            $users = $userRepository->findAll();
+            return $this->json($users, 200);
         } catch (\Exception $e) {
             return $this->json([
-                'error_message' => 'Missing essential key'
-            ], 400);
+                'error' => 'Server error'
+            ], 500);
         }
-
-        /* Check if essentials keys are not null ("" values) */
-        if (empty($email) || empty($password) || empty($firstName) || empty($lastName) || ($isPublicProfil === null) || empty($activeYears)) {
-            return $this->json([
-                'error_message' => 'A required field is missing'
-            ], 400);
-        } elseif (!is_string($email) || !is_string($password) || !is_string($firstName) || !is_string($lastName) || !is_bool($isPublicProfil) || !is_array($activeYears)) {
-            return $this->json([
-                'error_message' => 'One or more filled-in field(s) has/have a wrong type'
-            ], 400);
-        }
-
-
-
-        return $this->json([
-            'Content-Type' => 'ok'
-        ], 200);
-
-//        $postalCode = $request->query->get('postalCode');
-//
-//        $json = $request->getContent();
-//
-//        $user = $serializer->deserialize($json, User::class, 'json');
-//
-//        $errors = $validator->validate($user);
-//
-//        if (count($errors) > 0) {
-//            return new Response($serializer->serialize($errors, 'json'), 400, [
-//                'Content-Type' => 'application/json'
-//            ]);
-//        }
-//
-//        $user->setPassword(
-//            $userPasswordHasher->hashPassword(
-//                $user,
-//                json_decode($json)->password
-//            )
-//        );
-//        $entityManager->persist($user);
-//        $entityManager->flush();
-//
-//        $json = $serializer->serialize($user, 'json', ['groups' => 'user']);
-//
     }
 
-    #[Route('/{id}', name: 'app_api_user_patch', methods: ['PATCH'])]
-    public function updateUser(Request $request, SerializerInterface $serializer, ValidatorInterface $validator, User $user, EntityManagerInterface $entityManager): Response
+    // Get medias's users
+    #[Route('/medias', name: 'app_api_users_medias_get', methods: ['GET'])]
+    public function getUsersMedias(Request $request, UserRepository $userRepository): JsonResponse
     {
-        $json = $request->getContent();
+        try {
+            // Récupérer la limite et l'offset depuis les paramètres de requête
+            $limite = $request->query->get('limite', 30);
+            $offset = $request->query->get('offset', 0);
 
-        $user = $serializer->deserialize($json, User::class, 'json', ['object_to_populate' => $user]);
+            // Récupérer les utilisateurs avec la limite et l'offset
+            $users = $userRepository->findBy([], ['id' => 'ASC'], $limite, $offset);
 
-        $errors = $validator->validate($user);
+            // Créer un tableau avec les valeurs de profilPicture
+            $profilPictures = [];
+            foreach ($users as $user) {
+                $profilPictures[] = $user->getProfilPicture();
+            }
 
-        if (count($errors) > 0) {
-            return new Response($serializer->serialize($errors, 'json'), 400, [
-                'Content-Type' => 'application/json'
-            ]);
+            // Retourner le tableau en JSON
+            return $this->json($profilPictures, 200);
+        } catch (\Exception $e) {
+            return $this->json([
+                'error' => 'Server error'
+            ], 500);
         }
-
-        $entityManager->persist($user);
-        $entityManager->flush();
-
-        $json = $serializer->serialize($user, 'json', ['groups' => 'user']);
-
-        return new Response($json, 200, [
-            'Content-Type' => 'application/json'
-        ]);
     }
 
+    // Get one user
+    #[Route('/{id}', name: 'app_api_user_get_one', methods: ['GET'])]
+    public function getUserById(UserRepository $userRepository, int $id): JsonResponse
+    {
+        try {
+            $user = $userRepository->find($id);
+
+            if (!$user) {
+                return $this->json([
+                    'error' => 'User not found'
+                ], 404);
+            }
+
+            return $this->json($user, 200);
+        } catch (\Exception $e) {
+            return $this->json([
+                'error' => 'Server error'
+            ], 500);
+        }
+    }
+
+    // Create user
+    #[Route('/register', name: 'app_api_user_post', methods: ['POST'])]
+    public function register(Request $request, SerializerInterface $serializer, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): JsonResponse
+    {
+        try {
+            $content = json_decode($request->getContent(), true);
+
+            if (empty($content)) {
+                return $this->json([
+                    'error' => 'No data provided'
+                ], 400);
+            }
+
+            $requiredFields = ['email', 'password', 'firstName', 'lastName', 'isPublicProfil', 'activeYears'];
+
+            // Vérification de la présence de tous les champs requis
+            foreach ($requiredFields as $field) {
+                if (!isset($content[$field])) {
+                    return $this->json([
+                        'error' => "Missing field '$field'"
+                    ], 400);
+                }
+            }
+
+            // Vérification des types des champs requis
+            if (!is_string($content['email']) || !is_string($content['password']) || !is_string($content['firstName']) || !is_string($content['lastName']) || !is_bool($content['isPublicProfil']) || !is_array($content['activeYears'])) {
+                return $this->json([
+                    'error' => 'One or more filled-in field(s) has/have a wrong type'
+                ], 400);
+            }
+
+            $email = $content['email'];
+
+            // Vérification de l'existence de l'utilisateur
+            $existingUser = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+            if ($existingUser !== null) {
+                return $this->json([
+                    'error' => 'Email already exists'
+                ], 400);
+            }
+
+            $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $content['password']
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->json($user, 201);
+        } catch (\Exception $e) {
+            return $this->json([
+                'error' => 'Server error'
+            ], 500);
+        }
+    }
+
+    // Update user
+    #[Route('/{id}', name: 'app_api_user_update', methods: ['PATCH'])]
+    public function updateUser(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager, int $id): JsonResponse
+    {
+        try {
+            $user = $entityManager->getRepository(User::class)->find($id);
+
+            // Vérification de l'existence de l'utilisateur
+            if ($user === null) {
+                return $this->json([
+                    'error' => 'User not found'
+                ], 404);
+            }
+
+            $content = json_decode($request->getContent(), true);
+
+            if (empty($content)) {
+                return $this->json([
+                    'error' => 'No data provided'
+                ], 400);
+            }
+
+            foreach ($content as $key => $value) {
+                // Vérification de l'existence de la propriété dans l'objet User
+                if (!property_exists(User::class, $key)) {
+                    return $this->json([
+                        'error' => "Unknown property '$key'"
+                    ], 400);
+                }
+
+                $setter = 'set' . ucfirst($key);
+
+                // Vérification de l'existence de la méthode setter pour la propriété
+                if (!method_exists(User::class, $setter)) {
+                    return $this->json([
+                        'error' => "No setter found for property '$key'"
+                    ], 500);
+                }
+
+                // Appel de la méthode setter pour modifier la propriété
+                $user->$setter($value);
+            }
+
+            $entityManager->flush();
+
+            return $this->json($user, 200);
+        } catch (\Exception $e) {
+            return $this->json([
+                'error' => 'Server error'
+            ], 500);
+        }
+    }
+
+    // Delete user
     #[Route('/{id}', name: 'app_api_user_delete', methods: ['DELETE'])]
-    public function deleteUser(User $user, EntityManagerInterface $entityManager): Response
+    public function deleteUser(User $user = null, EntityManagerInterface $entityManager): Response
     {
-        $entityManager->remove($user);
-        $entityManager->flush();
+        try {
+            if (!$user) {
+                return $this->json([
+                    'error' => 'User not found'
+                ], 404);
+            }
 
-        return new Response(null, 204);
+            $entityManager->remove($user);
+            $entityManager->flush();
+
+            return $this->json([], 204);
+        } catch (\Exception $e) {
+            return $this->json([
+                'error' => 'Server error'
+            ], 500);
+        }
     }
 }
