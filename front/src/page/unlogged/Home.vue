77f0ace2d70c0b6165dc2824765239ad957b7210
Rongier Charles <charles.rongier@etu.univ-lyon1.fr>
2023-03-30 09:01:53 +0200

    timeline month

--- front/src/page/unlogged/Home.vue
+++ front/src/page/unlogged/Home.vue
@@ -5,13 +5,42 @@
     export default {
         name: 'home',
         data: () => ({ timelineSteps: [] }),       
-        computed: {            
+        computed: {   
+            month: function(){
+
+            }         
         },
-        methods: {      
+        methods: {
+            getMonthFromDateString(dateString) {
+                const monthNumber = dateString.substring(5, 7);
+                const months = [
+                    'Janvier',
+                    'Février',
+                    'Mars',
+                    'Avril',
+                    'Mai',
+                    'Juin',
+                    'Juillet',
+                    'Août',
+                    'Septembre',
+                    'Octobre',
+                    'Novembre',
+                    'Décembre'
+                ];
+                return months[parseInt(monthNumber, 10) - 1] + ' ' + dateString.substring(0,4);
+            }
         },
         mounted() {    
             axios.get('https://127.0.0.1:8000/timelinesteps').then(response => {
                 console.log(response.data);
+                for (let i = 0; i < response.data.length -1; i++) {
+                    if(response.data[i].date > response.data[i+1].date){
+                        let tmp = response.data[i];
+                        response.data[i] = response.data[i+1];
+                        response.data[i+1] = tmp;
+                        i=0;
+                    }
+                }
                 this.timelineSteps = response.data;
                 console.log("test");
             });        
@@ -27,7 +56,13 @@
     <main>
     <h1>Home</h1>
     <ul v-for="timelineStep in timelineSteps" :key="timelineStep.id">
+        <div v-if="timelineStep.media_id" class="stepWithPicture">
+        </div>
+        <div v-else class="stepWithoutPicture">
+            <h2> {{ timelineStep.title }}</h2>
+            <p>{{ getMonthFromDateString(timelineStep.date) }}</p>
             {{ timelineStep.content }}
+        </div>
     </ul>
     </main>
     
@@ -35,5 +70,7 @@
 </template>
 
 <style scoped>
-
+    ul div{
+        border: 1px solid black;
+    }
 </style>
