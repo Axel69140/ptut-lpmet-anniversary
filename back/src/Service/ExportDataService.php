4e5770169a85beba3dd7e04fdcc569ee168ce992
Axel_69140 <>
2023-04-27 16:33:15 +0200

    participant controller

--- back/src/Service/ExportDataService.php
+++ back/src/Service/ExportDataService.php
@@ -2,13 +2,15 @@
 
 namespace App\Service;
 
+use App\Entity\Anecdote;
 use App\Repository\AnecdoteRepository;
+use Doctrine\Common\Util\ClassUtils;
+use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
 
 class ExportDataService
 {
 
     public function exportAllCSV(AnecdoteRepository $anecdoteRepository){
-        $header_args = ["Object", "ID"];
 
         header('Content-Type: text/csv; charset=utf-8');
         header('Content-Disposition: attachment; filename=csv_export.csv');
@@ -17,15 +19,33 @@ class ExportDataService
 
         ob_end_clean();
 
-        fputcsv($output, $header_args);
-
         $datas = [];
 
         $anecdotes = $anecdoteRepository->findAll();
 
+        $header_args = [];
+        dd(get_class_vars());
+        foreach (get_object_vars($anecdotes[0]) as $key => $value) {
+            array_push($header_args, $key);
+        }
+
+        fputcsv($output, $header_args);
+
         foreach ($anecdotes as $anecdote)
         {
-            array_push($datas, [$anecdote->getId(), $anecdote->getContent(), $anecdote->isValidate()]);
+            $anecdoteDatas = [];
+            foreach ((array)$anecdote as $value) {
+                if(gettype($value) === 'object')
+                {
+                    if(ClassUtils::getClass($value) === 'App\Entity\User')
+                    {
+                        array_push($anecdoteDatas, '(' . $value->getId() . ') ' . $value->getFirstName() . ' ' . $value->getLastName());
+                    }
+                } else {
+                    array_push($anecdoteDatas, (string)$value);
+                }
+            }
+            array_push($datas, $anecdoteDatas);
         }
 
         foreach($datas AS $data){
