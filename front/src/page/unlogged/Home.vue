1aea4c89941b03cd3e844de1182642bda8d3d7dd
Cleaux Nathan <nathan.cleaux@etu.univ-lyon1.fr>
2023-05-03 11:17:55 +0200

    ok

--- front/src/page/unlogged/Home.vue
+++ front/src/page/unlogged/Home.vue
@@ -52,7 +52,6 @@
 
 <template>
     <main>
-        <h1>Timeline</h1>
         <ul class="allTimelineSteps">
             <li v-for="timelineStep in timelineSteps" :key="timelineStep.id" class="stepWithBorder">
                 <div v-if="timelineStep.media_id" class="stepWithPicture">
@@ -90,104 +89,107 @@
 </template>
 
 <style scoped>
-    *{
-        margin: 0;
-        padding: 0;
-        border: none;
-    }
+*{
+    margin: 0;
+    padding: 0;
+    border: none;
+}
 
-    .stepWithoutPicture{
-        background-color: #fff;
-        border: 5px solid var(--third);
-        border-radius: 10px;
-        width: 90%;
-        padding: 15px;
-        margin: 0 20px;
-    }
+.stepWithoutPicture{
+    background-color: #fff;
+    border: 5px solid var(--third);
+    border-radius: 10px;
+    width: 90%;
+    padding: 15px;
+    margin: 0 20px;
+}
 
-    .stepWithPicture{
-        background-color: #fff;
-        border: 5px solid var(--third);
-        border-radius: 10px;
-        width: 90%;
-        padding: 15px;
-        margin: 0 20px;
-    }
+.stepWithPicture{
+    background-color: #fff;
+    border: 5px solid var(--third);
+    border-radius: 10px;
+    width: 90%;
+    padding: 15px;
+    margin: 0 20px;
+}
 
-    li{
-        max-width: 50%;
-        display: flex;
-        flex-direction: column;
-    }
+li{
+    max-width: 50%;
+    display: flex;
+    flex-direction: column;
+}
 
-    li:nth-child(odd){
-        display: flex;
-        flex-direction: row-reverse;
-        margin-left: 50%;
-    }
+li:nth-child(odd){
+    display: flex;
+    flex-direction: row-reverse;
+    margin-left: 50%;
+}
 
-    li:nth-child(even){
-        margin-right: 50%;
-    }
+li:nth-child(even){
+    margin-right: 50%;
+}
 
-    .stepWithBorder{
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-    }
+.stepWithBorder{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
 
-    .dateStep{
-        color: var(--primary);
-    }
+.dateStep{
+    color: var(--primary);
+}
 
-    .spans{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        width: 1px;
-    }
-    span.barre{
-        content: "";
-        border: var(--primary) solid 4px;
-        height: 100%;
-    }
+.spans{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 1px;
+}
+span.barre{
+    content: "";
+    border: var(--primary) solid 4px;
+    height: 100%;
+}
 
-    span.cercle{
-        content: "";
-        border: solid 5px;
-        border-radius: 50px;
-        color: var(--primary);
-        background-color: var(--secondary);
-        min-width: 40px;
-        min-height: 40px;
-    }
+span.cercle{
+    content: "";
+    border: solid 5px;
+    border-radius: 50px;
+    color: var(--primary);
+    background-color: var(--secondary);
+    min-width: 40px;
+    min-height: 40px;
+}
 
-    .headerStep{
-        display: flex;
-        justify-content: space-between;
-    }
+.headerStep{
+    display: flex;
+    justify-content: space-between;
+}
 
-    .imageStep{
-        margin: 10px 20px 10px 0;
-        max-width: 250px;
-        max-height: 200px;
-    }
+.imageStep{
+    margin: 10px 20px 10px 0;
+    max-width: 250px;
+    max-height: 200px;
+}
 
-    .floatImage{
-        float: left;
-    }
+.floatImage{
+    float: left;
+}
 
-    .contentStep{
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        text-align: justify;
-    }
+.contentStep{
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: justify;
+}
 
-    .mainStep{
-        margin: 25px 0 0 0;
-    }
+.mainStep{
+    margin: 25px 0 0 0;
+}
 
-    .endCercle{
-        margin: 0 50%;
-    }
+.endCercle{
+    margin: 0 50%;
+}
 
+.allTimelineSteps {
+    margin-top: 100px;
+}
 </style>
