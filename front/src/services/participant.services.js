7676465aa3359dd548ef7444d91ba4e7d9da278c
Cleaux Nathan <nathan.cleaux@etu.univ-lyon1.fr>
2023-05-03 17:40:32 +0200

    ok

--- front/src/services/participant.services.js
+++ front/src/services/participant.services.js
@@ -12,9 +12,6 @@ const createParticipant = (participant) => {
     return Axios.post('https://127.0.0.1:8000/participants/create', participant);
 }
 
-const editParticipant = (participantMail, participant) => {    
-    return Axios.patch('https://127.0.0.1:8000/participants/' + participantMail, participant);
-}
 
 const deleteParticipants = (emails) => {    
     return Axios.delete('https://127.0.0.1:8000/participants/many', {
@@ -32,7 +29,6 @@ export const participantService = {
     getParticipants,
     getParticipantByEMail,
     createParticipant,
-    editParticipant,
     deleteParticipants,
     exportParticipantData
 }
